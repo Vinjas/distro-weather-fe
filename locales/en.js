437f2c2b103557e454fa937dac9f1b3bad970a25
Vinjas <danielvinyastorres@gmail.com>
2024-02-28 06:44:12 +0100

    feat: setup i18n to support multiple locales

---
+++ locales/en.js
@@ -0,0 +1,5 @@
+export default {
+  translation: {
+    'greeting': 'Hello, World!'
+  }
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './utils/i18n.js'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
