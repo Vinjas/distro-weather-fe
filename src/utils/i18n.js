df556b3ca54651fc66f69100952ff2dcedf9bbbe
Vinyas, Daniel <Daniel.Vinyas@adidas.com>
2024-02-28 06:44:12 +0100

    feat: setup i18n to support multiple locales

---
+++ src/utils/i18n.js
@@ -0,0 +1,21 @@
+
+// Import translations
+import en from '../../locales/en';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+const DEFAULT_LNG = 'en';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: { en },
+    lng: DEFAULT_LNG,
+    fallbackLng: DEFAULT_LNG,
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
+  export default i18n;
