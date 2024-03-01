4aeed349ab37ad57fae9dabd4007d1fac176cb0b
Vinjas <danielvinyastorres@gmail.com>
2024-03-01 06:54:18 +0100

    feat: update date range picker component and styles

--- src/components/date-range-picker/date-range-picker.jsx
+++ src/components/date-range-picker/date-range-picker.jsx
@@ -1,13 +1,16 @@
 import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css';
 import { useState } from 'react';
-import { DateRangePicker as ReactDateRange } from 'react-date-range';
+import { DateRangePicker as ReactDateRange, defaultStaticRanges, defaultInputRanges } from 'react-date-range';
 import { useSolarSearch } from '@contexts/solar-search-context';
 import { dateConvertToISO } from '@utils/date';
 import './date-range-picker.scss';
 
 export function DateRangePicker() {
   const { setCurrentStartDate, setCurrentEndDate } = useSolarSearch();
+
+  const customRanges = defaultStaticRanges.filter((range) => range.label !== 'This Month' && range.label !== 'This Week');
+  const customInputRanges = defaultInputRanges.filter((range) => range.label !== 'days starting today');
 
   const [selectedRange, setSelectedRange] = useState([
     {
@@ -28,6 +31,9 @@ export function DateRangePicker() {
     <div className='date-range-picker'>
     <ReactDateRange
         maxDate={new Date()}
+        fixedHeight
+        staticRanges={customRanges}
+        inputRanges={customInputRanges}
         ranges={selectedRange}
         onChange={handleSelect}
     />
